dc655ee138140b04600f1922ee1862505eb41043
Kevin <km09israel@gmail.com>
2026-09-04 16:10:16 -0500

    [ADD] facturacion 2.0

--- facturacion/app/Views/layouts/components/scripts.php
+++
@@ -1,8 +0,0 @@
-<!-- Bootstrap 5 JS Bundle CDN -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
-<!-- AdminLTE v4 JS (Local) -->
-<script src="<?= base_url('assets/adminlte/dist/js/adminlte.min.js') ?>"></script>
-
-<!-- Scripts adicionales por sección -->
-<?= $this->renderSection('scripts') ?>
